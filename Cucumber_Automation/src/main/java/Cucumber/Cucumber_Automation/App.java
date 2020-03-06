581c28db98c4ae2e1efd9336e7e296e170381374
waquar <waquar.alam92@gmail.com>
2020-03-06 10:00:39 +0530

    created skeleton

---
+++ Cucumber_Automation/src/main/java/Cucumber/Cucumber_Automation/App.java
@@ -0,0 +1,13 @@
+package Cucumber.Cucumber_Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
